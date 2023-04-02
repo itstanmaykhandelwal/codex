d18ef0bdee049754e4471f4ea9908de4e8fba824
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-02 15:26:15 +0530

    url fix

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://https://codex-lyxn.onrender.com/', {
+    const response = await fetch('https://codex-lyxn.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
