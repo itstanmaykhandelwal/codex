9c8a12ffa447fde9ae658dbf18646b5558face03
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-02 17:37:41 +0530

    api change

--- client/script.js
+++ client/script.js
@@ -87,6 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     const response = await fetch('https://codex-lyxn.onrender.com/', {
+    // const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
