3b8255eaea0857d6e8166cb4f6d6ec528ea7ba2b
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-02 15:19:22 +0530

    server is connected with vercel

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     // fetch from backend
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codex-lyxn.onrender.com',{
         method:'POST',
         headers:{
             'Content-Type':'application/json'
