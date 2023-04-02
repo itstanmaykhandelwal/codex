33aa6dac2478bab77f11636b6f330fa87838631a
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-02 19:04:42 +0530

    api change-2

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-lyxn.onrender.com/', {
+    const response = await fetch('https://codex-gpt-weg1.onrender.com/', {
     // const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
